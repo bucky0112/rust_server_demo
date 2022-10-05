0e1fecbe28defb439ca1141f06a9b8076a6b31db
bucky0112 <bucky0112@gmail.com>
2022-10-05 23:56:00 +0800

    add 404

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::{fs, path::Path};
 
 const PORT: i32 = 3000;
 
@@ -17,13 +17,19 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    let c = stream.read(&mut buffer).unwrap();
+    stream.read(&mut buffer).unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    println!("請求內容: {}", String::from_utf8_lossy(&buffer[..c]));
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let (status_line, path) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", Path::new("./src/HTML/hello.html"))
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", Path::new("./src/HTML/404.html"))
+    };
 
+    let contents = fs::read_to_string(path).unwrap();
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
         contents.len(),
         contents
     );
